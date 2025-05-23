5d9ba98f02623a3d2979a564df18091f7a7d104c
Salmon-ehab <ayoubsalma703@gmail.com>
2025-05-23 16:16:10 +0300

    final

--- lib/feature/settings/presentation/manager/change_language_manager/change_language_cubit.dart
+++ lib/feature/settings/presentation/manager/change_language_manager/change_language_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:login_task_nti/core/cache/cache_helper.dart';
 import 'package:login_task_nti/feature/settings/presentation/manager/change_language_manager/change_language_state.dart';
 
 class ChangeLanguageCubit extends Cubit<ChangeLanguageState> {
@@ -13,7 +12,7 @@ class ChangeLanguageCubit extends Cubit<ChangeLanguageState> {
   void changeLanguageFromMenu(String lang) async {
     // await getIt<CacheHelper>().setCurrentLanguage(language: lang);
     // await getIt<CacheHelper>()
-        .setCurrentLanguageCode(languageCode: languages[lang]!);
+        // .setCurrentLanguageCode(languageCode: languages[lang]!);
     emit(ChangeLanguageDone());
   }
 }
